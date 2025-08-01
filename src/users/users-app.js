a95a7b30805f0d93918d8f44818fa18ac281e037
root <root@Aorus.localdomain>
2025-08-01 02:27:59 -0400

    se implementa tabla

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,7 +1,14 @@
 import usersStore from "./store/users-store.js";
+import {RenderTable} from "./presentation/render-table/render-table.js";
 
-export const usersApp = ( element )=> {
+
+export const usersApp = async ( element )=> {
     element.innerHTML = 'Loading';
-    usersStore.loadNextPage();
+    await usersStore.loadNextPage();
+    element.innerHTML = '';
+
+    RenderTable( element );
+
+
 }
 
